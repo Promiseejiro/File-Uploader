4b4858f663b959e699257ca1946f115b48796fd9
Daniel Ikoyo <dannirolands@gmail.com>
2023-05-19 19:12:13 -0500

    fix issue of env variable returning undefined

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,12 +7,17 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./route/routes"));
 const db_1 = __importDefault(require("./db/db"));
-const port = 2000;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: __dirname + "/../.env" });
 const app = (0, express_1.default)();
-//medewares
+const port = process.env["PORT "] || 2000;
+const mongodbUrl = process.env["MONGODB_URI"];
+// check if the env is set, remember to delete this
+console.log(process.env[`MONGODB_URI`]);
+//medeware
 app.use((0, cors_1.default)());
 app.use("/", routes_1.default);
 app.listen(port, () => {
-    (0, db_1.default)("mongodb+srv://promise:4128@cluster0.x9stvxh.mongodb.net/");
+    (0, db_1.default)(mongodbUrl);
     console.log("app is listenig on port 2000");
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 import router from "./route/routes";
 import connectDb from "./db/db";
-const port = 2000;
+import dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/../.env" });
+
 const app = express();
-//medewares
+const port = process.env["PORT "] || 2000;
+const mongodbUrl = process.env["MONGODB_URI"];
+// check if the env is set, remember to delete this
+console.log(process.env[`MONGODB_URI`]);
+
+//medeware
 app.use(cors());
 app.use("/", router);
 app.listen(port, (): void => {
-  connectDb("mongodb+srv://promise:4128@cluster0.x9stvxh.mongodb.net/");
+  connectDb(mongodbUrl!);
   console.log("app is listenig on port 2000");
 });
